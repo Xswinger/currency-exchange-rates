7919465897311573b6a55a09ca001a96bf40c1b2
Xswinger <68491440+Xswinger@users.noreply.github.com>
2024-05-15 19:37:17 +0300

    Add: currency rate component; Update: replace stubs in app on currency-rate view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrencyRateComponent } from './components/currency-rate/currency-rate.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CurrencyRateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
